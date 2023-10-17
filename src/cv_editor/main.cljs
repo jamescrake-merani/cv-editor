9fc02fdcf534f562a82c33acfb196ebe44104b63
James Crake-Merani <crake-meranij@cardiff.ac.uk>
2023-10-17 22:25:14 +0100

    Create the base of the CV data structure.

--- src/cv_editor/main.cljs
+++ src/cv_editor/main.cljs
@@ -2,6 +2,13 @@
   [:require [reagent.core :as r]
    [reagent.dom :as rd]])
 
+(def initial-cv
+  {:full-name ""
+   :biography ""})
+
+(def current-cv
+  (r/atom initial-cv))
+
 (defn base []
   [:div
    [:p "Hello World!"]])
