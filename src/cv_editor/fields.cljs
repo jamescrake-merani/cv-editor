9904eecd0dd6c654684914452edc5e483c8ca5a8
James Crake-Merani <crake-meranij@cardiff.ac.uk>
2023-10-18 13:42:37 +0100

    First draft of fields namespace.

---
+++ src/cv_editor/fields.cljs
@@ -0,0 +1,20 @@
+(ns cv-editor.fields
+  [:require [reagent.core :as r]])
+
+(def field-component-map
+  {:full-name (cons full-name-view full-name-editor)})
+
+(defn full-name-view
+  [value callback]
+  [:div
+   [:h1.text-center value]
+   [:button {:on-click callback} "Change"]])
+
+(defn full-name-editor
+  [value callback]
+  (let [current-value (r/atom value)]
+    (fn []
+      [:div
+       [:p "Full Name"]
+       [:input {:type "text"}]
+       [:button {:on-click callback} "Save"]])))
